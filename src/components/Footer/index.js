7f77894493caf63cf336692d2df3df4d4130f1a6
Alisson Igor Freitas de Oliveira <alisson.ifo@outlook.com>
2020-07-28 16:10:33 -0300

    Aula 2.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,6 +13,8 @@ function Footer() {
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
+        {' '}
+        por Alisson Igor
       </p>
     </FooterBase>
   );
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import './'
@@ -8,11 +9,11 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="AlissonFlix Logo" />
-            </a>
+            </Link>
 
-            <Button href="href" as="a" className="ButtonLink">
+            <Button to="/cadastro/video" as={Link} className="ButtonLink">
                 Novo vídeo
             </Button>    
         </nav>
